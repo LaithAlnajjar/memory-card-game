70e16eab57750b0ef6e65d7da26ff183d94c0d2d
Laith Alnajjar <laithalnajjar2004@gmail.com>
2024-12-09 13:50:42 +0300

    feat: Implement function to shuffle cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,20 @@
 import '../styles/Card.css';
 
-export default function Card({ name, sprite }) {
+export default function Card({
+  name,
+  sprite,
+  shuffle,
+  id,
+  clicked,
+  setClicked,
+}) {
+  const handleClick = () => {
+    shuffle();
+    setClicked([...clicked, id]);
+  };
+
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       <img src={sprite} alt={name + ' Sprite'} />
       <p className="card-label">{name}</p>
     </div>
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -4,10 +4,11 @@ import '../styles/CardGrid.css';
 
 export default function CardGrid() {
   const [spriteList, setSpriteList] = useState([]);
+  const [clickedId, setClickedId] = useState([]);
 
   useEffect(() => {
-    //Array of pokemon that will be used in the URL send to the API
-    const pokemonArray = [
+    //spriteListay of pokemon that will be used in the URL send to the API
+    const pokemonspriteListay = [
       'charmander',
       'vulpix',
       'growlithe',
@@ -22,10 +23,10 @@ export default function CardGrid() {
       'bronzor',
     ];
 
-    //Recieves all fetch requests as Promises then resolves them returning an array containg a link to the image
+    //Recieves all fetch requests as Promises then resolves them returning an spriteListay containg a link to the image
     const getImages = async () => {
-      const spriteArray = await Promise.all(
-        pokemonArray.map(async (pokemon, index) => {
+      const spritespriteListay = await Promise.all(
+        pokemonspriteListay.map(async (pokemon, index) => {
           const response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${pokemon}`
           );
@@ -38,16 +39,35 @@ export default function CardGrid() {
         })
       );
 
-      setSpriteList(spriteArray);
+      setSpriteList(spritespriteListay);
     };
 
     getImages();
   }, []); // Function will only run once (on mount)
 
+  const shuffle = () => {
+    const shuffled = [...spriteList];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setSpriteList(shuffled);
+  };
+
   return (
     <div className="card-grid">
       {spriteList.map((card) => {
-        return <Card sprite={card.sprite} name={card.name} key={card.key} />;
+        return (
+          <Card
+            sprite={card.sprite}
+            name={card.name}
+            key={card.key}
+            id={card.key}
+            shuffle={shuffle}
+            clicked={clickedId}
+            setClicked={setClickedId}
+          />
+        );
       })}
     </div>
   );
